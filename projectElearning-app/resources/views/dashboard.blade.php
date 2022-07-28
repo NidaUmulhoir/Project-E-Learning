6247e21f1ba1370a6f5ddc86e94fb0c390515124
azizahcahya <zellchan07@gmail.com>
2022-07-28 15:57:38 +0700

    feat: user menu

---
+++ projectElearning-app/resources/views/dashboard.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/dashboard.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <title>Document</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-brand">
+           <img src="./asset/logo.png">
+        </div>
+
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <div class="menu">
+                        <img src="./asset/icon-1.png" alt="">
+                        <h3>Users</h3>
+                    </div>
+                </li>
+                <li>
+                    <div class="menu">
+                        <img src="./asset/icon-2.png" alt="">
+                        <h3>Payment</h3>
+                    </div>
+                </li>
+                <li>
+                    <div class="menu">
+                        <img src="./asset/icon-3.png" alt="">
+                        <h3>Course</h3>
+                    </div>
+                </li>
+                <li>
+                    <div class="menu">
+                        <img src="./asset/icon-4.png" alt="">
+                        <h3>Module</h3>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="header">
+        <div class="user-pro">
+            <img src="./asset/key-ico.png" alt="">
+            <h3>Admin</h3>
+            <img src="./asset/profile.png" alt="">
+        </div>
+    </div>
+    <hr>
+    <div class="main-content">
+        <h2>Dashboards Users</h2>
+        <div class="member">
+            <div class="member-pre">
+                <h1>150</h1>
+                <h4>Member Premium</h4>
+                <img src="./asset/user-ico.png" alt="">
+                <button class="info">More Info <i class='bx bx-right-arrow-circle'></i></button>
+            </div>
+            <div class="member-reg">
+                <h1>150</h1>
+                <h4>Member Regular</h4>
+                <img src="./asset/user-ico.png" alt="">
+                <button class="info">More Info <i class='bx bx-right-arrow-circle'></i></button>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
