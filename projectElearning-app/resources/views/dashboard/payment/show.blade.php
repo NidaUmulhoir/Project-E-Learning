1068491d3af72840ff90ac466a15e191b6e94688
azizahcahya <zellchan07@gmail.com>
2022-08-19 08:47:09 +0700

    styled page

--- projectElearning-app/resources/views/dashboard/payment/show.blade.php
+++ projectElearning-app/resources/views/dashboard/payment/show.blade.php
@@ -1,4 +1,26 @@
+@extends('dashboard.layouts.main')
 
-@if ($payment->image)
-    <img src="{{ asset('storage/' . $payment->image) }}" alt="">
-@endif
+@section('container')
+<br><br>
+<a href="/admin/payment">
+    <button type="admin" class="btn btn-primary">Back</button>
+</a>
+<br>
+<h3>Bukti Pembayaran</h3>
+<table>
+    <tr>
+        <td>Name    :</td>
+        <td></td>
+        <td>{{ $payment->user->name }}</td>
+    </tr>
+
+    <tr>
+        <td>Packet  :</td>
+        <td></td>
+        <td>{{ $payment->packet->packet }}</td>
+    </tr>
+</table>
+    @if ($payment->image)
+        <img src="{{ asset('storage/' . $payment->image) }}" alt="">
+    @endif
+@endsection
